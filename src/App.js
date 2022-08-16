12f52b98fe9578c40cff1c8ea1d0339bcf8c54a5
Matheus Mulundu <61382592+iam-matheus@users.noreply.github.com>
2022-08-16 16:35:00 +0200

    uploading first progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
-function App() {
+// my api link
+const API_URL = "http://www.omdbapi.com?apikey=8ac6d8a9";
+
+//the movie object
+const movie1 = {
+  Title: "Superman, Spiderman or Batman",
+  Year: "2011",
+  imdbID: "tt2084949",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
+};
+
+const App = () => {
+  //search movies function
+  const searchMovies = async (title) => {
+    let response = await fetch(`${API_URL}&s=${title}`);
+    let data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  //fetch movies ffrom the database
+  useEffect(() => {
+    searchMovies("spiderman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      {/* search tab */}
+      <div className="search">
+        <input
+          placeholder="Search for a movie.."
+          value="Spiderman"
+          onChange={() => {}}
+        />
+
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container"></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const MovieCard = () => {
+  return <React.Fragment></React.Fragment>;
+};
+
+export default MovieCard;
